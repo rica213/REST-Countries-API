7cf7494d35e46dd352eb6f1984d4da7fc5a34a8e
Andrea <andrearivas4cinfor@gmail.com>
2023-03-05 20:16:44 +0100

    finish search country function

--- src/modules/search.js
+++ src/modules/search.js
@@ -1,36 +1,47 @@
-
 const formSearch = document.querySelector('.search-country-form');
 const inputSearch = document.querySelector('.search-country-input');
 
 
 export const getDomelements = () =>{
   const countryOfficial = document.querySelectorAll('.official-name');
- return countryOfficial;
+  return countryOfficial;
 }
+
 const convertInput = (str) => {
- const strConverted =  str.charAt(0).toUpperCase() + str.slice(1);
- return strConverted
+  const strConverted =  str.charAt(0).toUpperCase() + str.slice(1);
+  return strConverted
 }
-
-
-
-const displaySearched = (countryInput,countriesDom) => {
-  countriesDom.forEach((country)=>{
+const displaySearched = (countryInput, countriesDom) => {
+  const countries = Array.from(countriesDom);
+  countries.forEach((country) => {
     const countryOfficial = country.textContent;
-    if(countryOfficial.includes(countryInput)) {
-      country.parentElement.parentElement.classList.add('found');
+    let score = 0;
+    if (countryOfficial.includes(countryInput)) {
+      score = countryOfficial.split(countryInput).join('').length;
+      country.parentElement.parentElement.style.display = "block";
     } else {
-      country.parentElement.parentElement.classList.add('hidden')
-  
+      country.parentElement.parentElement.classList.add('hidden');
     }
-  })
+    country.dataset.score = score;
+  });
+  countries.sort((a, b) => b.dataset.score - a.dataset.score);
+  const countriesContainer = document.querySelector('.countries-container');
+  countries.forEach((country) => {
+    countriesContainer.appendChild(country.parentElement.parentElement);
+  });
 }
+
+
+inputSearch.addEventListener('input', (e) => {
+  const countryInput = convertInput(e.target.value);
+  const countriesDom = getDomelements();
+  displaySearched(countryInput, countriesDom);
+});
 
 formSearch.addEventListener('submit', (e) => {
   e.preventDefault()
   const countryInput = convertInput(inputSearch.value);
   const countriesDom = getDomelements();
-  displaySearched(countryInput,countriesDom)
+  displaySearched(countryInput, countriesDom);
   formSearch.reset();
 });
-
