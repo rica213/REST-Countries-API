0b47724c6739a952188dc83247dbc2c7094b54fc
Andrea <andrearivas4cinfor@gmail.com>
2023-03-07 17:31:08 +0100

    html markup of details page

--- src/modules/country-details.js
+++ src/modules/country-details.js
@@ -2,12 +2,30 @@ const detailsSection = document.querySelector(".detailed-country");
 const contriesSection = document.querySelector('.countries');
 
 const createDetails = (name) => {
-  console.log(name)
+  const img = name.querySelector('.flag');
+  const { src, alt } = img;
+  const officialName = name.querySelector('.official-name').textContent;
+  
   let html = '';
-  html +=  `
-  <div>
-  ${name}
-  </div>`
+  html += `
+  <button class="back-btn">Back</button>
+  <div class="details-container-country">
+  <img src=${src} class="flag-details" alt=${alt}>
+  <div class="country-full-details">
+  <h2 class="official-name-det"></h2>
+  <div class="country-details-text">
+  <p class="details-text"><span>Native Name</span></p>
+  <p class="details-text"><span>Population</span></p>
+  <p class="details-text"><span>Region</span></p>
+  <p class="details-text"><span>Sub Region</span></p>
+  <p class="details-text"><span>Capital</span></p>
+  <p class="details-text"><span>Top Level Domain</span></p>
+  <p class="details-text"><span>Euro</span></p>
+  <p class="details-text"><span>Languages</span></p>
+  </div>
+  <div class="border-countries"></div>
+  </div>
+  </div>`;
   detailsSection.innerHTML = html
 }
 
