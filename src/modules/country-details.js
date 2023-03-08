79828b644415278012a9eb12c3cc7818acf1d0a5
Andrea <andrearivas4cinfor@gmail.com>
2023-03-08 19:43:44 +0100

    improved code

--- src/modules/country-details.js
+++ src/modules/country-details.js
@@ -1,78 +1,69 @@
 import url from "./url.js";
 import retrieve from "./retrieve.js";
 
-
 const result = retrieve(url);
-
 const detailsSection = document.querySelector(".detailed-country");
 const countriesSection = document.querySelector(".countries");
 
-const createDetails = (officialName) => {
-  result.then((country) => {
-    country.forEach((country) => {
-      let html = "";
-      if(country.name.official === officialName) {
-        console.log(country);
-  
-        const nativeName = ()=> {
-          const values = Object.values(country.name.nativeName)
-          values.forEach((native)=> {
-            const nativeCommon = native.common;
-            console.log(nativeCommon)
-           return nativeCommon;
-          })
-          }
-        
-     
-         html += `
-          <button class="back-btn">Back</button>
-          <div class="details-container-country">
-            <img src="${country.flags.png}" alt="${country.flags.alt}" 
-            <div class="country-full-details">
-               <h2 class="official-name-det">${country.name.official}</h2>
-               <div class="country-details-text">
-                   <p class="details-text"><span>Native Name: </span>${Object.values(country.name.nativeName).map(native => native.common)}</p>
-                   <p class="details-text"><span>Population: </span>${
-                     country.population
-                   }</p>
-                   <p class="details-text"><span>Region: </span>${
-                     country.region
-                   }</p>
-                   <p class="details-text"><span>Sub Region: </span>${
-                     country.subregion
-                   }</p>
-                   <p class="details-text"><span>Capital: </span>${
-                     country.capital[0]
-                   }</p>
-                   <p class="details-text"><span>Top Level Domain: </span> ${
-                     country.tld
-                   }</p>
-                   <p class="details-text"><span>Currencies: </span>${Object.values(
-                     country.currencies
-                   ).map(coin=>coin.name)}</p>
-                   <p class="details-text"><span>Languages: </span>${Object.values(
-                     country.languages
-                   )}</p>
-  </div>
-  <div class="border-countries"></div>
-  </.>
-  </div>`;
+function createDetails(country) {
+  let html = `
+    <button class="back-btn">Back</button>
+    <div class="details-container-country">
+      <img src="${country.flags.png}" alt="${country.flags.alt}">
+      <div class="country-full-details">
+        <h2 class="official-name-det">${country.name.official}</h2>
+        <div class="country-details-text">
+          <p class="details-text"><span>Native Name: </span>${nativeName(
+            country
+          )}</p>
+          <p class="details-text"><span>Population: </span>${
+            country.population
+          }</p>
+          <p class="details-text"><span>Region: </span>${country.region}</p>
+          <p class="details-text"><span>Sub Region: </span>${
+            country.subregion
+          }</p>
+          <p class="details-text"><span>Capital: </span>${
+            country.capital[0]
+          }</p>
+          <p class="details-text"><span>Top Level Domain: </span>${
+            country.tld
+          }</p>
+          <p class="details-text"><span>Currencies: </span>${Object.values(
+            country.currencies
+          ).map((coin) => coin.name)}</p>
+          <p class="details-text"><span>Languages: </span>${Object.values(
+            country.languages
+          )}</p>
+        </div>
+        <div class="border-countries"></div>
+      </div>
+    </div>
+  `;
   detailsSection.innerHTML = html;
-      }
-    });
-  });
-};
+}
 
-const countryDetails = () => {
+function nativeName(country) {
+  return Object.values(country.name.nativeName)
+    .map((native) => native.common)
+    .join(", ");
+}
+
+function countryDetails() {
   const card = document.querySelectorAll(".countries li");
   card.forEach((country) => {
     country.addEventListener("click", () => {
       countriesSection.style.display = "none";
       detailsSection.style.display = "block";
-      const officialName = country.querySelector('.official-name').textContent;
-      createDetails(officialName);
+      const officialName = country.querySelector(".official-name").textContent;
+      result.then((countries) => {
+        const country = countries.find(
+          (country) => country.name.official === officialName
+        );
+        createDetails(country);
+      });
     });
   });
-};
+}
 
 export default countryDetails;
