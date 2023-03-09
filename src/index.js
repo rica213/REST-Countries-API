265852c7a62160fbe4ab3f375ccf52b9631fe804
Andrea <andrearivas4cinfor@gmail.com>
2023-03-09 17:51:08 +0100

    implement dark mode

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 import url from './modules/url.js';
 import retrieve from './modules/retrieve.js';
 import displayCountries from './modules/display-countries.js';
-
+import './modules/dark-mode.js'
 const container = document.querySelector('.countries ul');
 const result = retrieve(url);
 
---
+++ src/modules/dark-mode.js
@@ -0,0 +1,8 @@
+const darkModeBtn = document.querySelector(".dark-mode-btn");
+// const boqdy = document.querySelector('.countries ul');
+
+darkModeBtn.addEventListener('click', ()=> {
+
+  document.body.classList.add('dark-mode');
+
+})
