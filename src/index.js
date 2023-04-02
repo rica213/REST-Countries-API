70d5db6968f4fedaebd538bbdf9daec2c77ed9d0
Andrea <andrearivas4cinfor@gmail.com>
2023-04-02 18:49:42 +0200

    Add dark mode functionality

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 import displayCountries from './modules/display-countries.js';
 import filterByRegion from './modules/filter.js';
 import result from './modules/retrieve.js';
-
+import './modules/dark-mode'
 const filterInput = document.querySelector('.region-select');
 const container = document.querySelector('.countries ul');
 window.addEventListener('load', () => {
---
+++ src/modules/dark-mode.js
@@ -0,0 +1,7 @@
+const darkModeBtn = document.querySelector('.dark-mode-btn')
+// const boqdy = document.querySelector('.countries ul');
+
+darkModeBtn.addEventListener('click', () => {
+ document.body.classList.toggle('dark-mode');
+  
+})
